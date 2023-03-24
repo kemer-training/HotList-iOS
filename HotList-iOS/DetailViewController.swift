e8fa0311e53224ff190185ff3fea255fe5769f6d
Nahom Abraham <nahom@kemercode.com>
2023-03-24 17:00:44 +0300

    Add detailView controller in storyboard and detailViewController file, add layout to detailView in storyboard

---
+++ HotList-iOS/DetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailViewController.swift
+//  HotList-iOS
+//
+//  Created by NAHØM on 24/03/2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController{
+    
+    @IBOutlet weak var artworkView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var kindLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var releaseDateLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
